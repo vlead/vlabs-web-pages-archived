ba6593349c38aedf66f06def0e235d9c8f734acb
apurv111 <apurv@vlabs.ac.in>
2015-08-06 10:05:22 +0530

    resolve merge conflicts

--- src/sortLinks.js
+++ src/sortLinks.js
@@ -1,7 +1,17 @@
 window.onload = function () { sortLinks() }
 sortLinks = function () {
+	sortedLinks = []
+	parent = []
 	links = document.getElementsByClassName('sidebar-a')
 	for ( i = 9 ; i <= links.length - 1 ; i ++ ) {
-		alert(links[i].innerHTML)
+		sortedLinks.push(links[i])
+	}
+	
+	for ( i = 0 ; i < sortedLinks.length ; i ++ ) {
+		sortedLinks[i].innerHTML = sortedLinks[i].innerHTML.trim()
+	}
+	sortedLinks.sort()
+	for ( i = 0 ; i < sortedLinks.length ; i ++ ) {
+		parent.push(sortedLinks[i].parentNode.parentNode.parentNode)
 	}
 }
