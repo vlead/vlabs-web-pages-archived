85e4457ce852265b4c866751323bff23899bd909
apurv111 <apurv@vlabs.ac.in>
2015-08-06 20:58:08 +0530

    update javascript for add tarhet to anchor tags

--- src/tabs.js
+++ src/tabs.js
@@ -1,13 +1,18 @@
 function newTabs () {
 	var links = document.getElementsByClassName('lab-list-row-div')
 	for ( i = 0 ; i < links.length ; i ++ ) {
-		labLink = links[i].firstChild.nextSibling.firstChild.nextSibling
-		labLink.setAttribute('target', '_blank')
-		labLogo = links[i].firstChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling	
-		labLogo.setAttribute('target', '_blank')
-		lectureLogo = labLogo.nextSibling.nextSibling.firstChild.nextSibling
-		lectureLogo.setAttribute('target', '_blank')
-	}
+		anchors = links[i].getElementsByTagName('a')
+		for ( j = 0 ; j < anchors.length ; j ++ ) {
+			anchors[j].setAttribute('target', '_blank')
+		}
+ 	}
+ 	var footer = document.getElementsByClassName('border-right-green-dotted')
+ 	for ( i = 0 ; i < footer.length ; i ++ ) {
+ 		footerLinks = footer[i].getElementsByTagName('a')
+ 		for ( j = 0 ; j < footerLinks.length ; j ++ ) {
+ 			footerLinks[j].setAttribute('target', '_blank')
+ 		}
+ 	}
 }
 
 window.onload = function(){ newTabs() }
