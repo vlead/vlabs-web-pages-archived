e90036d0af9d22cc26e03f854da7e8780564f0aa
apurv111 <apurv@vlabs.ac.in>
2015-08-06 17:33:23 +0530

    added script to automate process of opening of links in a new tab

---
+++ src/tabs.js
@@ -0,0 +1,4 @@
+var links = document.getElementsByClassName('text-h3-darkblue').document.getElementsByTagName('a')
+	for ( i = 0 ; i < links.length ; i ++ ) {
+		links[i].setAttribute('target', '_blank')
+	}
