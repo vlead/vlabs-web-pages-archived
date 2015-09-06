654938d4420b044ba2cd115e6d7b043a1b832f87
Sonali-Goyal <charvi.s@practo.com>
2015-09-06 22:33:35 +0530

    Latest commit with google analytics

--- src/computer-graphics-iiith/src/lab/save_json.php
+++ src/computer-graphics-iiith/src/lab/save_json.php
@@ -3,54 +3,88 @@
     $filetxt = 'data.txt';
 
         // check if all form data are submited, else output error message
-        if(isset($_POST['element_1_1']) && isset($_POST['element_2']) && isset($_POST['element_3_1']) && isset($_POST['element_3_2']) && isset($_POST['element_3_1'])) {
-        // if form fields are empty, outputs message, else, gets their data
+       
         if(empty($_POST['element_1_1']) || empty($_POST['element_2']) || empty($_POST['element_3_1']) || empty($_POST['element_3_2']) || empty($_POST['element_3_3'])) {
             echo 'All fields are required';
         }
         else {
-        // gets and adds form data into an array
-        $data = array(
-          'Name'=> $_POST['element_1_1'],
-          'Email'=> $_POST['element_2'],
-          'Phn1'=> $_POST['element_3_1'],
-          'Phn2'=> $_POST['element_3_2'],
-          'Phn3'=> $_POST['element_3_3'],
-	  'Roll No'=> $_POST['element_4'],
-          'Month'=> $_POST['element_5_1'],
-          'Date'=> $_POST['element_5_2'],
-          'Year'=> $_POST['element_5_3'],
-	  'Inst Name'=> $_POST['element_6'],
-	  'Inst IP'=> $_POST['element_7'],
-	  'Domain'=> $_POST['element_8'],
-	  'Lab Name'=> $_POST['element_9'],		
-        );
 
-        // path and name of the file
-        $filetxt = 'data.txt';
+	$servername = "127.0.0.1:3306";
+	$username = "root";
+	$password = "";
+	$dbname = "feedback";
 
-        $arr_data = array();        // to store all form data
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 
-        // check if the file exists
-        if(file_exists($filetxt)) {
-          // gets json-data from file
-          $jsondata = file_get_contents($filetxt);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 
-          // converts json string into array
-          $arr_data = json_decode($jsondata, true);
-        }
+// Create database
+$sql = "CREATE DATABASE feedback";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+$conn = new mysqli($servername, $username, $password, "feedback");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sql="CREATE TABLE Feedback (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Name VARCHAR(30) NOT NULL,
+email VARCHAR(50) NOT NULL,
+phone INT(10) NOT NULL,
+rollno INT(10) NOT NULL,
+faculty INT(2) NOT NULL,
+student INT(2) NOT NULL,
+institute_name VARCHAR(100) ,
+institute_ip VARCHAR(100) NOT NULL,
+discipline VARCHAR(150) NOT NULL,
+labname VARCHAR(100) NOT NULL,
+experiment_name VARCHAR(100),
+issue VARCHAR(250),
+comments VARCHAR(250),
+question1 VARCHAR(500),
+question2 VARCHAR(500),
+question3 VARCHAR(500),
+question4 VARCHAR(500)
+)";
+if ($conn->query($sql) === TRUE) {
+    echo "Table feedback created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+$name=$_POST['element_1_1'];
+$email=$_POST['element_2'];
+$phone=$_POST['element_3_1']+$_POST['element_3_2']+$_POST['element_3_1'];
+$rollno=$_POST['element_4'];
+$faculty=1;
+$student=0;
+$institute_name=$_POST['element_6'];
+$institute_ip=$_POST['element_7'];
+$discipline=$_POST['element_8'];
+$labname=$_POST['element_9'];
+$experiment_name='ss';
+$issue=$_POST['element_10'];
+$comments=$_POST['element_11'];
+$question1=$_POST['element_12'];
+$question2=$_POST['element_13'];
+$question3=$_POST['element_14'];
+$question4=$_POST['element_15']; 
+     
+$sql = "INSERT INTO Feedback(Name,email,phone,rollno,faculty,student,institute_name,institute_ip,discipline,labname,experiment_name,issue,comments,question1,question2,question3,question4) 
+VALUES ($name, $email, $phone,$rollno,$faculty,$student,$institute_name,$institute_ip,$discipline,$labname,$experiment_name,$issue,$comments,$question1,$question2,$question3,$question4)";
 
-        // appends the array with new form data
-        $arr_data[] = $data;
-
-        // encodes the array into a string in JSON format (JSON_PRETTY_PRINT - uses whitespace in json-string, for human readable)
-        $jsondata = json_encode($arr_data, JSON_PRETTY_PRINT);
-
-        // saves the json string in "data.txt" (in "dirdata" folder)
-        // outputs error message if data cannot be saved
-        if(file_put_contents('data.txt', $jsondata)) echo 'Data successfully saved';
-        else echo 'Tidak dapat menyimpan data di "data.txt"';
-      }
-    }
-        else echo 'Form fields not submited';
-    ?>
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+}
+?>
